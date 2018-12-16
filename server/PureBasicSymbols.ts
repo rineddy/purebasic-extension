f574f9871a985828b0b47f7f1c1bd15abea18d7e
RinEddy <rineddy@gmail.com>
2018-12-16 13:38:17 +0100

    symbols

--- server/PureBasicSymbols.ts
+++ server/PureBasicSymbols.ts
@@ -23,15 +23,22 @@ export class PureBasicSymbols {
 	 * @param params
 	 */
 	public getDocumentSymbols(params: DocumentSymbolParams): SymbolInformation[] {
-		return [
-			SymbolInformation.create('Module', SymbolKind.Module, Range.create(0, 1, 6, 1), params.textDocument.uri),
-			SymbolInformation.create('Function', SymbolKind.Function, Range.create(1, 2, 2, 3), params.textDocument.uri, 'Module'),
-			SymbolInformation.create('Property', SymbolKind.Property, Range.create(3, 2, 3, 3), params.textDocument.uri, 'Module'),
+		let m = SymbolInformation.create('Module::', SymbolKind.Struct, Range.create(0, 1, 3, 1), params.textDocument.uri);
+		let a = SymbolInformation.create('a', SymbolKind.Field, Range.create(14, 2, 14, 3), params.textDocument.uri),
+			b = SymbolInformation.create('b', SymbolKind.Constant, Range.create(15, 2, 15, 3), params.textDocument.uri),
+			c = SymbolInformation.create('c', SymbolKind.Namespace, Range.create(8, 1, 9, 1), params.textDocument.uri);
+		a.containerName = m.name;
+		b.containerName = m.name;
+		c.containerName = m.name;
 
-			SymbolInformation.create('Module', SymbolKind.Struct, Range.create(14, 1, 19, 1), params.textDocument.uri, 'Module'),
-			SymbolInformation.create('Field', SymbolKind.Field, Range.create(14, 2, 14, 3), params.textDocument.uri, 'Module'),
-			SymbolInformation.create('Constant', SymbolKind.Constant, Range.create(15, 2, 15, 3), params.textDocument.uri, 'Module'),
-			SymbolInformation.create('Namespace', SymbolKind.Namespace, Range.create(8, 1, 9, 1), params.textDocument.uri),
+		let zz = SymbolInformation.create('zz', SymbolKind.Function, Range.create(20, 2, 22, 3), params.textDocument.uri),
+			zzz = SymbolInformation.create('zzz', SymbolKind.Property, Range.create(20, 2, 23, 3), params.textDocument.uri);
+		zz.containerName = m.name;
+		zzz.containerName = m.name;
+		return [
+			m,
+			a, b, c,
+			zz, zzz
 		];
 	}
 	/**
@@ -39,6 +46,18 @@ export class PureBasicSymbols {
 	 * @param params
 	 */
 	public getWorkspaceSymbols(params: WorkspaceSymbolParams): SymbolInformation[] {
+		let m = SymbolInformation.create('Module::', SymbolKind.Struct, Range.create(0, 1, 3, 1));
+		let a = SymbolInformation.create('a', SymbolKind.Field, Range.create(14, 2, 14, 3)),
+			b = SymbolInformation.create('b', SymbolKind.Constant, Range.create(15, 2, 15, 3)),
+			c = SymbolInformation.create('c', SymbolKind.Namespace, Range.create(8, 1, 9, 1));
+		a.containerName = m.name;
+		b.containerName = m.name;
+		c.containerName = m.name;
+
+		return [
+			m,
+			a, b, c
+		];
 		return [];
 	}
 }
